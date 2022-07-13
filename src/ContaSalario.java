6ae790622279891a3f4ed1ed5d755970bb8b28f2
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-07-13 19:50:15 -0300

    Implementado saque especializado para ContaSalario()

--- src/ContaSalario.java
+++ src/ContaSalario.java
@@ -10,5 +10,13 @@ public class ContaSalario extends Conta{
 		System.out.println("CONTA SALÁRIO");
 		super.imprimeInfo();
 	}
+	
+	public void saca(double valor) {
+		if (valor > this.saldo) {
+			System.out.println("Saldo insuficiente.\nContas do tipo salário não tem limite.");
+			return;
+		}
+		this.saldo -= valor;
+	}
 
 }
